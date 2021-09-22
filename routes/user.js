a4ca99d2b70524ec562e027f9a90de0642c111d0
Abdullahi Adam <AbdullahiAdam.aa@gmail.com>
2021-09-22 18:34:15 +0100

    implement user endpoint

--- routes/user.js
+++ routes/user.js
@@ -40,4 +40,14 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
